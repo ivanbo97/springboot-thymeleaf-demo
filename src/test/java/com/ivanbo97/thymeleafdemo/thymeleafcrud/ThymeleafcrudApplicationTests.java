06217c582b0adb9d77427a58f5bdd2531d6ca735
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-30 09:07:17 +0300

    Initial commit

---
+++ src/test/java/com/ivanbo97/thymeleafdemo/thymeleafcrud/ThymeleafcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanbo97.thymeleafdemo.thymeleafcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
