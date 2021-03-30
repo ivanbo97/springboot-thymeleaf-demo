b12c12a9c909f57cd65969771079bb9e7fb2f48d
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-30 13:25:31 +0300

    Adding mapping for POST request.

--- src/main/java/com/ivanbo97/thymeleafdemo/thymeleafcrud/controller/EmployeeController.java
+++ src/main/java/com/ivanbo97/thymeleafdemo/thymeleafcrud/controller/EmployeeController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
 
 import com.ivanbo97.thymeleafdemo.thymeleafcrud.entity.Employee;
 import com.ivanbo97.thymeleafdemo.thymeleafcrud.service.EmployeeService;
 import static com.ivanbo97.thymeleafdemo.thymeleafcrud.util.ApplicationConstants.EMPLOYEES_BASE_URL;
-import java.util.List;
+import static com.ivanbo97.thymeleafdemo.thymeleafcrud.util.ApplicationConstants.EMPLOYEES_FORM_URL;
+import static com.ivanbo97.thymeleafdemo.thymeleafcrud.util.ApplicationConstants.EMPLOYEES_FORM_SAVE_URL;
 
 @Controller
 public class EmployeeController {
@@ -27,5 +31,22 @@ public class EmployeeController {
 		
 		theModel.addAttribute("employees", employees);
 		return "employee-list";
+	}
+	
+	@GetMapping(EMPLOYEES_FORM_URL)
+	public String showEmployeeFrom(Model theModel) {
+		
+		Employee employee = new Employee();
+		
+		theModel.addAttribute("employee", employee);
+		
+		return "employee-form";
+	}
+	
+	@PostMapping(EMPLOYEES_FORM_SAVE_URL)
+	public String saveEmployee(@ModelAttribute("employee")Employee employee) {
+		
+		employeeService.save(employee);
+		return "redirect:"+EMPLOYEES_BASE_URL;
 	}
 }
