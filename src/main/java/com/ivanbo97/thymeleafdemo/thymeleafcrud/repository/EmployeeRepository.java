b6c30141b078c413821cd78818c36cb79540a31a
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-03-30 13:26:00 +0300

    Adding method for ordering employees by last name.

--- src/main/java/com/ivanbo97/thymeleafdemo/thymeleafcrud/repository/EmployeeRepository.java
+++ src/main/java/com/ivanbo97/thymeleafdemo/thymeleafcrud/repository/EmployeeRepository.java
@@ -1,12 +1,16 @@
 package com.ivanbo97.thymeleafdemo.thymeleafcrud.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ivanbo97.thymeleafdemo.thymeleafcrud.entity.Employee;
 
 
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
